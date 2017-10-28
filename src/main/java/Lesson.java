59f7ec0da659f5a7d8198adde2bf190d3fc4307e
Mindi <slaw.malcz@wp.pl>
2017-10-28 18:13:06 +0200

    Creating method saveProgres in Lesson class.
    Now it'll save all progres to file .txt with data as a name

--- src/main/java/Lesson.java
+++ src/main/java/Lesson.java
@@ -1,3 +1,4 @@
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -22,6 +23,19 @@ public class Lesson {
         return output;
     }
 
+    public Boolean saveProgres(){
+        try{
+            PrintWriter file = new PrintWriter(getName()+".txt","UTF-8");
+            file.write(toString());
+            file.close();
+            return true;
+        }catch(Exception e){
+            System.out.println("Error during creating response file");
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
     public void setAnswers(int id,String newExercise){
         this.getAnswers().get(id).setRespond(newExercise);
     }
@@ -32,7 +46,7 @@ public class Lesson {
         return this.answers.get(ID);
     }
     private void setName(){
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
         Date date = new Date();
         this.name = dateFormat.format(date);
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
         number1.setAnswers(0,"Ala ma kota ale kot nie slucha ali");
 
         System.out.println(number1.toString());
+        number1.saveProgres();
     }
 }
